422410ce6cedaed6873c4c3ac8dbec1e2edca2d5
Fola <k043635@eltham-college.org.uk>
2024-11-06 08:03:04 +0000

    overlayed floor plan on map

--- NEA/js/script.js
+++ NEA/js/script.js
@@ -8,7 +8,26 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 const floorplanBounds = [
-    [51.43847553199154, 0.037628591359193685]
-
+    [51.438803, 0.037470],
+    [51.438183, 0.039517]
 ]
 
+fetch("../floorplan/floorplan.svg")
+    .then(response => response.text())
+    .then(svgText => {
+        // Create a div container to hold the SVG content
+        const svgContainer = L.DomUtil.create('div');
+        svgContainer.innerHTML = svgText;
+
+        const svgElement = svgContainer.querySelector('svg');
+        svgElement.setAttribute("transform", "rotate(-15)");  // Adjust angle as needed
+        svgElement.setAttribute("transform-origin", "center center");
+
+
+        // Create a Leaflet overlay that will hold the SVG container
+        const svgOverlay = L.svgOverlay(svgContainer, floorplanBounds, {
+            opacity: 0.4,
+            interactive: true
+        }).addTo(map);
+    })
+    .catch(error => console.error("Error loading SVG:", error));
